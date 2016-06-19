6bd4019a271d919c59afc1102cbe087999abcbd2
laishi <laishi886@hotmail.com>
2016-06-19 09:27:38 +0800

    add home conf.js

---
+++ public/lib/js/look.conf.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+
+    var homejs = [
+
+        'public/incude/vue/vue-router.min.js',
+        'public/incude/vue/vue.min.js',
+        'public/incude/vue/vueItem.js',
+
+        'public/incude/greensock/TweenMax.min.js',
+        'public/lib/js/expandItem.js',
+
+        'public/lib/js/scrollheader.js',
+        'public/lib/js/sliderPage.js',
+        'public/lib/js/usedetail.js',
+        'public/lib/js/scrollgrid.js',
+
+        'public/incude/mixitup/js/jquery.mixitup.min.js',
+        'public/incude/slinky/js/slinkyHome.js',
+        'public/incude/highlight/js/highlight.pack.js',
+
+    ]
+
+
+
+
+
+    for (var i = 0; i < homejs.length; i++) {  
+        loadjscssfile(homejs[i], "js", "body");
+    };
+
+
+function loadjscssfile(filename, filetype, pos) {
+    var fileref;
+
+    if (filetype === "js") { //if filename is a external JavaScript file
+        fileref = document.createElement("script");
+        fileref.setAttribute("type", "text/javascript");
+        fileref.setAttribute("src", filename);
+    }
+    else if (filetype === "css") { //if filename is an external CSS file
+        fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+    }
+
+    if (fileref) {
+        document.getElementsByTagName(pos)[0].appendChild(fileref);
+    }
+}
+
+
+
+});
